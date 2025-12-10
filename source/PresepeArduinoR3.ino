891948433bb3129330d6d45238cc577870b0ca79
Pietro Rotundo <pietro.rt85@gmail.com>
2025-12-10 18:44:34 +0100

    definizione pin output pwm

--- source/PresepeArduinoR3.ino
+++ source/PresepeArduinoR3.ino
@@ -1,5 +1,9 @@
+int stellaCometaPin = 3;        // PWM pin
+int cieloPin = 5;               // PWM pin
 
 void setup() {
+    pinMode(stellaCometaPin, OUTPUT);
+    pinMode(cieloPin, OUTPUT);
 }
 
 void loop() {
