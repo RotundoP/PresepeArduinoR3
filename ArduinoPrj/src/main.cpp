13f2d2e6d67a002f5f7ae75a652d3f30c44d9cd5
Pietro Rotundo <pietro.rt85@gmail.com>
2025-12-10 23:48:17 +0100

    Utilizzo classe TimerClass

--- ArduinoPrj/src/main.cpp
+++ ArduinoPrj/src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
 #include <header.h>
+#include <TimerClass.h>
+
 
 int CometStarPwmPin = 3;       
 int SkyPwmPin = 5;               
 int CometStarFadeVal, SkyFadeVal; 
+TimerClass MyTontime(10);
 
 void setup() {
+    
     Serial.begin(9600);
     pinMode(CometStarPwmPin, OUTPUT);
     pinMode(SkyPwmPin, OUTPUT);
@@ -16,13 +20,16 @@ void setup() {
 void loop() {
     //analogWrite(CometStarPwmPin,500);
     
-    delay(500);
-    increaseFade(&CometStarFadeVal, -10);
+    delay(200);
+    increaseFade(&CometStarFadeVal, -5);
     Serial.println("CometStar value: ");
     Serial.print(CometStarFadeVal);
     Serial.println();
+
+    //MyTontime.TonTimer(true);
     //digitalWrite(LED_BUILTIN, LOW);
     //delay(500);
+    //TON_longDelay(30);
    
 }
 
@@ -39,3 +46,17 @@ void increaseFade(int* value, int increaseUnit)
     
     *value = val;   
 }
+/*
+/// @brief long acyclic TON delay [s]
+/// @param value 
+/// @return return expired delay status
+bool TON_longDelay(bool enable, int value)
+{
+    if (enable)
+    {
+
+    }
+    else
+        return false;
+}
+        */
