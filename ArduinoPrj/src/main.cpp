e43664017a48a7c4d8b678ddea51a660bf84af85
Pietro Rotundo <pietro.rt85@gmail.com>
2025-12-10 22:09:14 +0100

    sistemazione codice

---
+++ ArduinoPrj/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+int stellaCometaPin = 3;        // PWM pin
+int cieloPin = 5;               // PWM pin
+
+void setup() {
+    pinMode(stellaCometaPin, OUTPUT);
+    pinMode(cieloPin, OUTPUT);
+}
+
+void loop() {
+    // commento
+}
