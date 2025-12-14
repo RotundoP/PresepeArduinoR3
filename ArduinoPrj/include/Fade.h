a9634219e9944c655a48cfb368b852fda81d8c86
Pietro Rotundo <pietro.rt85@gmail.com>
2025-12-14 22:59:15 +0100

    nuova classe Fade

---
+++ ArduinoPrj/include/Fade.h
@@ -0,0 +1,33 @@
+#ifndef Fade_H
+#define Fade_H
+
+    class PointXY
+    {
+        public:
+            float X;
+            float Y;
+    };
+
+    class Fade
+    {
+        public:
+            Fade(PointXY p[5]);
+            float UpdateCam(float masterVal);
+
+        private:
+            float c0, c1, c2, c3;
+            float y0, max_y, min_y;
+    };
+
+    Fade::Fade(PointXY p[5])
+    {
+        p[0].X;
+        p[0].Y;
+    }
+
+    float Fade::UpdateCam(float masterVal)
+    {
+        return 0.0;
+    }
+
+#endif
