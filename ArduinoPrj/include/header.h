ea6a6881358aa7109a497ce8556f6c4eab5f3a72
Pietro Rotundo <pietro.rt85@gmail.com>
2026-02-15 23:01:53 +0100

    Aggiunge enum sound effect

--- ArduinoPrj/include/header.h
+++ ArduinoPrj/include/header.h
@@ -1,2 +1,16 @@
 void increaseFade(int* value, int increaseUnit);
 void masterUpdate();
+
+enum soundEffect {
+  ROOSTER_1 = 1,
+  ROOSTER_2 = 2,
+  DONKEY = 3,
+  CAMEL = 4,
+  DOG = 5,
+  HORSE = 6,
+  CHICKEN = 7,
+  COW = 8,
+  DUCK = 10,
+  STORM = 3,
+  TUNDER = 4,
+}; 
